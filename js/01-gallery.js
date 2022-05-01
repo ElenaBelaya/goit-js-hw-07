b6f93fae130cbd2758010a9f3e1a7f5d03a57b46
ElenaBelaya <makseniya@ukr.net>
2022-05-01 18:10:24 +0300

    first task is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,24 +3,54 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-const galleryDiv = document.querySelector(`.gallery`);
-function createGalleryItem() {
-    const newGalleryEl = galleryItems.map((item) => {
-        return
-        `<div class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
+const galleryContainer = document.querySelector(`.gallery`);
+
+function createGalleryItem(gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
-            src="${item.preview}"
-            data-source="href="${item.original}""
-            alt="${item.description}"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
           />
         </a>
-      </div>`
-      console.log(newGalleryEl);
-    }).join("");
-
+      </div>   
+    `}).join("");
 }
-//const readyGallery = createGalleryItem(galleryItems);
-//console.log(readyGallery);
-//galleryDiv.insertAdjacentHTML("beforeend", newGalleryEl);
+
+const galleryCardMarkup = createGalleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardMarkup);
+
+galleryContainer.addEventListener(`click`, onLargeCardLink);
+
+function onLargeCardLink(e) {
+  e.preventDefault();
+  const cardLinkEl = e.target.closest('.gallery__image');
+  const cardLink = cardLinkEl.dataset.source;
+
+  const instance = basicLightbox.create(`
+  <div class="modal">
+     <img src=${cardLink} />
+  </div>
+  `, {
+    onShow: (instance) => {
+      instance.element().querySelector(`.modal`).onclick = instance.close;
+
+      window.addEventListener(`keydown`, onEscClose);
+    },
+    onClose: (instance) => {
+      window.removeEventListener(`keydown`, onEscClose);
+    }
+  })
+function onEscClose(event) {
+  if(event.code === "Escape") {
+    instance.close()
+  }
+}
+
+instance.show()
+}
